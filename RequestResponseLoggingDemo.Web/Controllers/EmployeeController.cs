622eb7a73584a8df4ef98dfc19eb9eee703e8f26
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-12-31 10:21:14 -0700

    Upgraded the project to .NET 5.0

--- RequestResponseLoggingDemo.Web/Controllers/EmployeeController.cs
+++ RequestResponseLoggingDemo.Web/Controllers/EmployeeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RequestResponseLoggingDemo.Web.Models;
+using System;
 
 namespace RequestResponseLoggingDemo.Web.Controllers
 {
--- RequestResponseLoggingDemo.Web/Middleware/RequestResponseLoggingMiddleware.cs
+++ RequestResponseLoggingDemo.Web/Middleware/RequestResponseLoggingMiddleware.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
